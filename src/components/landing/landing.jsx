5aa8653ad4d0dda21be74873887642d7b166af25
Jessica Ouyang <jessica.ouyang@gmail.com>
2021-02-08 10:14:30 -0800

    basic landing

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -4,7 +4,23 @@ import style from './style.css';
 
 const Landing = ({}) => {
 
-  return <div>wlecome to ditto bnb!!! </div>
+  return <div>
+    <div className={style.hero}>
+      <div className={style.nav}>
+        <div className={style.logo}>dittobnb</div>
+        <div className={style.links}>
+          <div>Places to stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+        <div>Become a host</div>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  </div>
 }
 
 export default Landing;
