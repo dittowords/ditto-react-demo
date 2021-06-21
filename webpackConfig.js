10e2385cc3b02cd17356fbfca432d55b48969477
Jordin Gardner <gard.jordin@gmail.com>
2021-06-21 10:11:25 -0700

    Revert "REVERT ME: temporary hack to test using published test package"
    This reverts commit d7fa81314576595d9d712ac2d52e577d233fca79.

--- webpackConfig.js
+++ webpackConfig.js
@@ -138,9 +138,6 @@ module.exports = (env, argv) => {
         'node_modules',
         path.resolve(__dirname, 'node_modules'),
       ],
-      alias: {
-        "ditto-react": path.resolve(__dirname, "node_modules/@jordin/react-library-testing")
-      }
     },
     output: {
       path: path.join(__dirname, 'dist'),
