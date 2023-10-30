31845a6d84d58c80a224e77c98d5fa9dd910c048
asnewman <ashznewman@gmail.com>
2023-10-30 15:10:16 -0700

    New cli generation files

--- src/ditto/index.js
+++ src/ditto/index.js
@@ -1,6 +1,9 @@
-module.exports = {
+import components__sample_components__base from './components__sample-components__base.json';
+import components__sample_components__portuguese___br_sample from './components__sample-components__portuguese---br-sample.json';
+
+export default {
   "ditto_component_library": {
-    "base": {...require('./components__sample-components__base.json')},
-    "portuguese---br-sample": {...require('./components__sample-components__portuguese---br-sample.json')}
+    "base": {...components__sample_components__base},
+    "portuguese---br-sample": {...components__sample_components__portuguese___br_sample}
   }
 }
