e9f2adde26b81ae52c2a5e46edd22e36159f73dc
Kaden Barlow <kadenbarlow@gmail.com>
2024-01-31 16:18:10 -0700

    small type and error handling improvements

--- src/ditto/helpers.ts
+++ src/ditto/helpers.ts
@@ -1,9 +1,12 @@
 import variables from "./variables.json";
 
-export function dittoVariableLink(key: string) {
+export function dittoVariableLink(key: keyof typeof variables) {
   const variable = (variables as any)[key];
   if (!variable) {
     throw new Error(`No Ditto Variable found: ${key}`);
+  }
+  if (!variable.url) {
+    throw new Error(`No URL found for Ditto Variable: ${key}`);
   }
 
   return variable.url;
